7b1a620a8574dd87256549643ea3d5a8d8b5584a
Eduard Frigola Bagué <eduardfrigola@yahoo.com>
2019-10-15 17:42:52 +0200

    enable watchdog

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,24 @@
 int main( ){
 //    ofSetupOpenGL(700,400,OF_WINDOW);            // <-------- setup the GL context
     
-    // ofxWatchdog::boot(msec, reboot, override, verbose)
+//     ofxWatchdog::boot(msec, reboot, override, verbose)
     //
     //     msec : how long does the watchdog wait, when the process hangs-up in milli seconds
     //   reboot : automatically restart the process
     // override : use internal signal handler (optional)
     //  verbose : print more log information (optional)
-//    ofxWatchdog::boot(3000, true, true, true);
+    ofxWatchdog::boot(3000, true, true, true);
     
     ofGLFWWindowSettings mainSettings;
     mainSettings.setSize(1280, 400);
     mainSettings.setPosition(glm::vec2(500,0));
     mainSettings.windowMode = OF_WINDOW;
-    mainSettings.resizable = true;
+    mainSettings.resizable = false;
     mainSettings.setGLVersion(4,1);
     ofCreateWindow(mainSettings);
     
     // ofxWatchdog::trap(void)
-//    ofxWatchdog::trap();
+    ofxWatchdog::trap();
     
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
